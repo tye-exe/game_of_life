07c497cac982484ad1a33c964674cde8313787ad
tye-exe <tye@mailbox.org>
2024-09-27 12:45:06 +0100

    Added first test - failing

--- src/logic/mod.rs
+++ src/logic/mod.rs
@@ -13,8 +13,45 @@ pub struct Position {
     y: u16,
 }
 
+impl Position {
+    pub fn new(x: u16, y: u16) -> Self {
+        Self { x, y }
+    }
+}
+
 /// A cell on the board.
+#[derive(Debug, PartialEq)]
 pub enum Cell {
     Alive,
     Dead,
+}
+
+impl Board {
+    pub fn set(&self, position: Position, cell: Cell) {
+        todo!()
+    }
+
+    pub fn get(&self, position: Position) -> Cell {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    impl Default for Board {
+        fn default() -> Self {
+            todo!()
+        }
+    }
+
+    #[test]
+    /// Sets a cell to be alive
+    fn set_cell_alive() {
+        let board = Board::default();
+        board.set(Position::new(0, 0), Cell::Alive);
+        let cell = board.get(Position::new(0, 0));
+        assert_eq!(cell, Cell::Alive);
+    }
 }
