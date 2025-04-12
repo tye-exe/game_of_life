7dec6b1855de3da3eaa2f7a506955a88e7891128
tye-exe <tye@mailbox.org>
2025-04-12 09:04:58 +0100

    Blueprint and Save verification
    Blueprint are safe files are checked that there size is as expected before being loaded.

--- gol_lib/src/persistence/load.rs
+++ gol_lib/src/persistence/load.rs
@@ -7,6 +7,18 @@ use super::{
     preview::{BlueprintPreview, SavePreview},
 };
 
+/// Returns true if the given number is equal to the given usize.
+fn usize_eq<Num>(unsigned_size: usize, other_num: Num) -> bool
+where
+    Num: TryFrom<usize> + PartialEq,
+{
+    let Ok(converted): Result<Num, _> = unsigned_size.try_into() else {
+        return false;
+    };
+
+    converted == other_num
+}
+
 /// The possible errors when attempting to parse a save file from disk.
 #[derive(thiserror::Error, Debug)]
 pub enum SaveParseError {
@@ -14,6 +26,13 @@ pub enum SaveParseError {
     FileRead(#[from] std::io::Error),
     #[error("File is not a valid save file")]
     InvalidData(#[from] serde_json::Error),
+    #[error(
+        "Simulation data does not match the area allocated for this board. 'data: {data_area}' : 'allocated: {allocated_area}'"
+    )]
+    UnexpectedSize {
+        data_area: usize,
+        allocated_area: u128,
+    },
 }
 
 /// The possible errors when attempting to parse a blueprint from disk.
@@ -23,12 +42,34 @@ pub enum BlueprintParseError {
     FileRead(#[from] std::io::Error),
     #[error("File is not a valid blueprint file")]
     InvalidData(#[from] serde_json::Error),
+    #[error(
+        "Blueprint cell data does not match the area allocated for this blueprint. 'data: {data_size}' : 'allocated: {allocated_size}'"
+    )]
+    UnexpectedSize {
+        data_size: usize,
+        allocated_size: u128,
+    },
+    #[error("The size of this blueprint is too large for your computer.")]
+    BlueprintTooBig,
 }
 
 /// Attempts to parse the board data from a save at the given file path.
 pub fn load_board_data(save_location: impl AsRef<Path>) -> Result<SimulationSave, SaveParseError> {
     let file = std::fs::File::open(save_location)?;
-    Ok(serde_json::from_reader(file)?)
+    let simulation: SimulationSave = serde_json::from_reader(file)?;
+
+    let area: u128 =
+        simulation.board_area.x_difference() as u128 * simulation.board_area.y_difference() as u128;
+    let unsigned_size = simulation.board_data.len();
+
+    if !usize_eq(unsigned_size, area) {
+        return Err(SaveParseError::UnexpectedSize {
+            data_area: unsigned_size,
+            allocated_area: area,
+        });
+    }
+
+    Ok(simulation)
 }
 
 /// Attempts to parse a blueprint from the file at the given path.
@@ -36,7 +77,19 @@ pub fn load_blueprint(
     blueprint_location: impl AsRef<Path>,
 ) -> Result<SimulationBlueprint, BlueprintParseError> {
     let file = std::fs::File::open(blueprint_location)?;
-    Ok(serde_json::from_reader(file)?)
+    let blueprint: SimulationBlueprint = serde_json::from_reader(file)?;
+
+    let area = blueprint.x_size as u128 * blueprint.y_size as u128;
+    let unsigned_size = blueprint.blueprint_data.len();
+
+    if !usize_eq(unsigned_size, area) {
+        return Err(BlueprintParseError::UnexpectedSize {
+            data_size: unsigned_size,
+            allocated_size: area,
+        });
+    }
+
+    Ok(blueprint)
 }
 
 /// Finds and parses [`SavePreview`]s from the given directory.
@@ -128,4 +181,160 @@ pub(crate) fn load<'a, Data: DeserializeOwned>(
         .collect();
 
     Ok(parsed_data)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::UNIX_EPOCH;
+
+    use bitvec::vec::BitVec;
+    use tempfile::tempdir;
+
+    use crate::{
+        Area,
+        persistence::{
+            SaveBuilder, SimulationBlueprint, SimulationSave,
+            load::{BlueprintParseError, SaveParseError},
+        },
+    };
+
+    use super::load_board_data;
+
+    #[test]
+    fn valid_blueprint() {
+        let temp_dir = tempdir().expect("Able to create temp dir");
+
+        let name = "abc";
+        let description = "efg";
+        let tags = Box::new(["hi"]);
+        let time = UNIX_EPOCH;
+
+        let path = SaveBuilder::new_blueprint(SimulationBlueprint {
+            x_size: 1,
+            y_size: 3,
+            blueprint_data: {
+                let mut bit_vec = BitVec::new();
+                bit_vec.push(true);
+                bit_vec.push(true);
+                bit_vec.push(true);
+                bit_vec.into_boxed_bitslice()
+            },
+        })
+        .name(name)
+        .desciprtion(description)
+        .tags(tags)
+        .time(time)
+        .save(temp_dir.path())
+        .expect("Able to save blueprint");
+
+        let simulation_blueprint = super::load_blueprint(path).expect("Can load blueprint");
+
+        assert_eq!(
+            simulation_blueprint,
+            SimulationBlueprint {
+                x_size: 1,
+                y_size: 3,
+                blueprint_data: {
+                    let mut bit_vec = BitVec::new();
+                    bit_vec.push(true);
+                    bit_vec.push(true);
+                    bit_vec.push(true);
+                    bit_vec.into_boxed_bitslice()
+                }
+            },
+            "The parsed blueprint does not match the expected blueprint."
+        )
+    }
+
+    #[test]
+    fn bad_blueprint() {
+        let temp_dir = tempdir().expect("Unable to create tempoary directory.");
+
+        let path = SaveBuilder::new_blueprint(SimulationBlueprint {
+            x_size: 2,
+            y_size: 2,
+            blueprint_data: {
+                let mut bit_vec = BitVec::new();
+                bit_vec.push(true);
+                bit_vec.into_boxed_bitslice()
+            },
+        })
+        .save(temp_dir.path())
+        .expect("Can save blueprint");
+
+        assert!(
+            matches!(
+                super::load_blueprint(path),
+                Err(BlueprintParseError::UnexpectedSize { .. })
+            ),
+            "The blueprint must fail to parse as the data size does not match the allocated size."
+        );
+    }
+
+    #[test]
+    fn valid_board() {
+        let temp_dir = tempdir().expect("Able to create temp dir");
+
+        let path = SaveBuilder::new_save(SimulationSave {
+            generation: 0,
+            board_area: Area::new((0, 0), (2, 2)),
+            board_data: {
+                let mut bit_vec = BitVec::new();
+                bit_vec.push(true);
+                bit_vec.push(true);
+                bit_vec.push(true);
+                bit_vec.push(true);
+                bit_vec.into_boxed_bitslice()
+            },
+        })
+        .save(temp_dir.path())
+        .expect("Able to save file");
+
+        let simulation_save = load_board_data(path).expect("Valid board data");
+
+        assert_eq!(
+            simulation_save,
+            SimulationSave {
+                generation: 0,
+                board_area: Area::new((0, 0), (2, 2)),
+                board_data: {
+                    let mut bit_vec = BitVec::new();
+                    bit_vec.push(true);
+                    bit_vec.push(true);
+                    bit_vec.push(true);
+                    bit_vec.push(true);
+                    bit_vec.into_boxed_bitslice()
+                },
+            },
+            "The parsed save does not match the expected save data."
+        )
+    }
+
+    #[test]
+    fn invalid_board() {
+        let temp_dir = tempdir().expect("Able to create temp dir");
+
+        let path = SaveBuilder::new_save(SimulationSave {
+            generation: 0,
+            board_area: Area::new((0, 0), (2, 1)),
+            board_data: {
+                let mut bit_vec = BitVec::new();
+                bit_vec.push(true);
+                bit_vec.push(true);
+                bit_vec.push(true);
+                bit_vec.push(true);
+                bit_vec.into_boxed_bitslice()
+            },
+        })
+        .save(temp_dir.path())
+        .expect("Able to save file");
+
+        assert!(
+            matches!(
+                load_board_data(path),
+                Err(SaveParseError::UnexpectedSize { .. })
+            ),
+            "Save must fail to parse as the data size does not match the allocated size."
+        )
+    }
 }
